ab8195a63de6e8885c24dab0bd011a76b471f61d
Timur Dzhesur <timurjes@gmail.com>
2023-10-21 22:59:45 +0200

    Base structure and new Header component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://land-maze.github.io',
+    site: 'https://landmaze.me',
 });
