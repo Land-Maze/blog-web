bf9b84813671c94f8ff322a68253498d4095b0fb
Timur Dzhesur <61432929+Land-Maze@users.noreply.github.com>
2024-02-10 02:48:05 +0100

    Polished everywhere I could see

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -3,30 +3,30 @@ export type ColorClasses = {
   };
   
 export const colors: ColorClasses = {
-    "red": ["bg-red-300", "text-red-900"],
-    "orange": ["bg-orange-300", "text-orange-900"],
-    "yellow": ["bg-yellow-300", "text-yellow-900"],
-    "green": ["bg-green-300", "text-green-900"],
-    "blue": ["bg-blue-300", "text-blue-900"],
-    "indigo": ["bg-indigo-300", "text-indigo-900"],
-    "violet": ["bg-violet-300", "text-violet-900"],
-    "pink": ["bg-pink-300", "text-pink-900"],
-    "turquoise": ["bg-turquoise-300", "text-turquoise-900"], // #40E0D0
-    "gold": ["bg-gold-300", "text-gold-900"], // #FFD700
-    "silver": ["bg-silver-300", "text-silver-900"], // C0C0C0
-    "brown": ["bg-brown-300", "text-brown-900"], // #964B00
-    "maroon": ["bg-maroon-300", "text-maroon-900"], // #800000
-    "olive": ["bg-olive-300", "text-olive-900"], // #808000
-    "lime": ["bg-lime-300", "text-lime-900"], 
-    "teal": ["bg-teal-300", "text-teal-900"],  
-    "peach": ["bg-peach-300", "text-peach-900"], // #FFE5B4
-    "magenta": ["bg-magenta-300", "text-magenta-900"], // #FF00FF
-    "lavender": ["bg-lavender-300", "text-lavender-900"], // #E6E6FA
-    "cyan": ["bg-cyan-300", "text-cyan-900"], 
-    "beige": ["bg-beige-300", "text-beige-900"], // #F5F5DC
-    "salmon": ["bg-salmon-300", "text-salmon-900"], // #fa8072
-    "khaki": ["bg-khaki-300", "text-khaki-900"], // #F0E68C
-    "rose": ["bg-rose-300", "text-rose-900"],
-    "plum": ["bg-plum-300", "text-plum-900"], // #DDA0DD
-    "slategray": ["bg-slateGray-300", "text-slateGray-900"], // #708090
+    "red": ["bg-red-200", "text-red-900"],
+    "orange": ["bg-orange-200", "text-orange-900"],
+    "yellow": ["bg-yellow-200", "text-yellow-900"],
+    "green": ["bg-green-200", "text-green-900"],
+    "blue": ["bg-blue-200", "text-blue-900"],
+    "indigo": ["bg-indigo-200", "text-indigo-900"],
+    "violet": ["bg-violet-200", "text-violet-900"],
+    "pink": ["bg-pink-200", "text-pink-900"],
+    "turquoise": ["bg-turquoise-200", "text-turquoise-900"], // #40E0D0
+    "gold": ["bg-gold-200", "text-gold-900"], // #FFD700
+    "silver": ["bg-silver-200", "text-silver-900"], // C0C0C0
+    "brown": ["bg-brown-200", "text-brown-900"], // #964B00
+    "maroon": ["bg-maroon-200", "text-maroon-900"], // #800000
+    "olive": ["bg-olive-200", "text-olive-900"], // #808000
+    "lime": ["bg-lime-200", "text-lime-900"], 
+    "teal": ["bg-teal-200", "text-teal-900"],  
+    "peach": ["bg-peach-200", "text-peach-900"], // #FFE5B4
+    "magenta": ["bg-magenta-200", "text-magenta-900"], // #FF00FF
+    "lavender": ["bg-lavender-200", "text-lavender-900"], // #E6E6FA
+    "cyan": ["bg-cyan-200", "text-cyan-900"], 
+    "beige": ["bg-beige-200", "text-beige-900"], // #F5F5DC
+    "salmon": ["bg-salmon-200", "text-salmon-900"], // #fa8072
+    "khaki": ["bg-khaki-200", "text-khaki-900"], // #F0E68C
+    "rose": ["bg-rose-200", "text-rose-900"],
+    "plum": ["bg-plum-200", "text-plum-900"], // #DDA0DD
+    "slategray": ["bg-slateGray-200", "text-slateGray-900"], // #708090
 };
