35be1411cc7774c945c500c3e6ba8f5b0da9910c
Timur Dzhesur <61432929+Land-Maze@users.noreply.github.com>
2024-03-10 19:46:35 +0100

    Created popup of first 5 posts, linked search library

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,6 +1,8 @@
 import type { PostDataType } from '../utils/types/MarkDown.ts'
+import { colors } from '../utils/colors';
+import { colorPickerLetter } from '../utils/colorPickerLetter.ts'
 import { searchFirstFive, type SearchResultType } from "../utils/searchPosts.ts"
-import { useEffect, useState, type FormEventHandler, type FormEvent } from "react"
+import { useEffect, useState, type FormEventHandler, type FormEvent, type MouseEventHandler, type MouseEvent } from "react"
 
 type SearchBarProps = {
   data: PostDataType[];
@@ -9,8 +11,8 @@ type SearchBarProps = {
 export default function SearchBar(props: SearchBarProps) {
 
   const [search, setSearch] = useState("")
-  const [searchResults, setSearchResults] = useState<SearchResultType[]>([])
-
+  const [searchResults, setSearchResults] = useState<SearchResultType[]>(searchFirstFive("ma", props.data))
+  
   useEffect(() => {
     if (search.length > 0){
       setSearchResults(searchFirstFive(search, props.data))
@@ -26,19 +28,32 @@ export default function SearchBar(props: SearchBarProps) {
   };
 
   const eventFocusHandler: FormEventHandler<HTMLInputElement> = (e: FormEvent<HTMLInputElement>) => {
-    const searchInput = document.getElementById("search-results")! as HTMLInputElement;
-
+    const searchBox = document.getElementById("search-results")! as HTMLInputElement;
+    const searchInput = document.getElementById("search-bar")! as HTMLInputElement;
+    
     if (search.length > 0){
     }
-    searchInput.classList.remove("hidden")
-    searchInput.classList.add("flex")
+    searchInput.classList.remove("rounded-xl")
+    searchInput.classList.add("rounded-t-xl")
+    
+    searchBox.classList.remove("hidden")
+    searchBox.classList.add("flex")
+  };
+  
+  const eventBlurHandler: FormEventHandler<HTMLInputElement> = (e: FormEvent<HTMLInputElement>) => {
+    const searchBox = document.getElementById("search-results")! as HTMLInputElement;
+    const searchInput = document.getElementById("search-bar")! as HTMLInputElement;
+
+
+    searchInput.classList.remove("rounded-t-xl")
+    searchInput.classList.add("rounded-xl")
+
+    searchBox.classList.remove("flex")
+    searchBox.classList.add("hidden")
   };
 
-  const eventBlurHandler: FormEventHandler<HTMLInputElement> = (e: FormEvent<HTMLInputElement>) => {
-    const searchInput = document.getElementById("search-results")! as HTMLInputElement;
+  const eventClickHandler: MouseEventHandler<HTMLDivElement> = (e: MouseEvent<HTMLDivElement>) => {
 
-    searchInput.classList.remove("flex")
-    searchInput.classList.add("hidden")
   };
 
   return(
@@ -49,8 +64,18 @@ export default function SearchBar(props: SearchBarProps) {
         </svg>
         <input onFocus={eventFocusHandler} onBlur={eventBlurHandler} onInput={eventInputHandler} value={search} type="text" autoComplete='off' id="search-input" placeholder="Search" className="text-black focus:outline-none ml-1 xl:text-2xl" />
       </div>
-      <div id="search-results" autoFocus className="flex-col bg-white p-2 rounded-b-xl items-center absolute w-full ">
-
+      <div id="search-results" autoFocus className="flex-col text-white p-0 rounded-b-xl items-center absolute z-10 w-full">
+        {searchResults.map((result, index) => {
+          const tagColor = colorPickerLetter(result.PostDataType.tags[0][0])
+          return(
+            <div key={"search-" + index} onClick={eventClickHandler} className='flex flex-col
+            border-2 border-t-0 first:border-t-0 bg-background-shades-600 border-background-shades-400 w-full text-sm cursor-pointer 
+            hover:scale-105 transition-all p-2 '>
+              <span>{result.PostDataType.title}</span>
+              <span className={`self-end text-2xs w-fit px-1 rounded-lg mt-1 ${colors[tagColor][0]} ${colors[tagColor][1]}`}>{result.PostDataType.tags[0]}</span>
+            </div>
+          )
+        })}
       </div>
     </form>
 )};
